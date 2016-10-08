dd1b011d191ff68af88102f50bf9da48e3c72c86
Milko Galev <milkopg@yahoo.com>
2016-10-08 23:45:42 +0300

    Leaving possibility for deletion of tables

--- app/src/main/java/project/android/softuni/bg/androiddetective/fragment/menu/SettingsFragment.java
+++ app/src/main/java/project/android/softuni/bg/androiddetective/fragment/menu/SettingsFragment.java
@@ -7,7 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import project.android.softuni.bg.androiddetective.R;
+import project.android.softuni.bg.androiddetective.util.Constants;
+import project.android.softuni.bg.androiddetective.webapi.model.Contact;
+import project.android.softuni.bg.androiddetective.webapi.model.ResponseObject;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +22,12 @@ public class SettingsFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
+
+//    List<ResponseObject> cameras= ResponseObject.find(ResponseObject.class, Constants.BROADCAST_NAME + "=?", Constants.RECEIVER_CAMERA);
+//    for (ResponseObject responseObject : cameras){
+//      responseObject.delete();
+//    }
+
     return inflater.inflate(R.layout.fragment_settings, container, false);
   }
 }
